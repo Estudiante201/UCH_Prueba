cfabfb5fdbc5b88f810c2fb4214ec46724d26f02
Estudiante201 <edwgordilloq@uch.pe>
2020-05-16 01:10:24 -0500

    Mi primer commit

---
+++ src/main/java/com/redsocial/DefSotPruebaGitApplication.java
@@ -0,0 +1,13 @@
+package com.redsocial;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DefSotPruebaGitApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DefSotPruebaGitApplication.class, args);
+	}
+
+}
